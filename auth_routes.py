e06e4041fc4bb12008ecb866bb787d00e0f27a86
jod35 <jodestrevin@gmail.com>
2021-09-06 21:13:35 +0300

    signing up

--- auth_routes.py
+++ auth_routes.py
@@ -1,4 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,status
+from database import Session,engine
+from schemas import SignUpModel
+from models import User
+from fastapi.exceptions import HTTPException
+from werkzeug.security import generate_password_hash , check_password_hash
 
 auth_router=APIRouter(
     prefix='/auth',
@@ -7,7 +12,45 @@ auth_router=APIRouter(
 )
 
 
+session=Session(bind=engine)
+
 @auth_router.get('/')
 async def hello():
     return {"message":"Hello World"}
+
+
+@auth_router.post('/signup',
+    status_code=status.HTTP_201_CREATED
+)
+async def signup(user:SignUpModel):
+    db_email=session.query(User).filter(User.email==user.email).first()
+
+    if db_email is not None:
+        return HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User with the email already exists"
+        )
+
+    db_username=session.query(User).filter(User.username==user.username).first()
+
+    if db_username is not None:
+        return HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User with the username already exists"
+        )
+
+    new_user=User(
+        username=user.username,
+        email=user.email,
+        password=generate_password_hash(user.password),
+        is_active=user.is_active,
+        is_staff=user.is_staff
+    )
+
+    session.add(new_user)
+
+    session.commit()
+
+    return new_user
+
+
+
 
---
+++ schemas.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class SignUpModel(BaseModel):
+    id:Optional[int]
+    username:str
+    email:str
+    password:str
+    is_staff:Optional[bool]
+    is_active:Optional[bool]
+
+
+    class Config:
+        orm_mode=True
+        schema_extra={
+            'example':{
+                "username":"johndoe",
+                "email":"johndoe@gmail.com",
+                "password":"password",
+                "is_staff":False,
+                "is_active":True
+            }
+        }
