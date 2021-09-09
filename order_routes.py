388492425689d3c983b0245afacd60eb19974066
jod35 <jodestrevin@gmail.com>
2021-09-09 06:25:56 +0300

    placing orders

--- order_routes.py
+++ order_routes.py
@@ -1,4 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Depends,status
+from fastapi.exceptions import HTTPException
+from fastapi_jwt_auth import AuthJWT
+from models import User,Order
+from schemas import OrderModel
+from database import Session , engine
+from fastapi.encoders import jsonable_encoder
 
 order_router=APIRouter(
     prefix="/orders",
@@ -6,6 +12,59 @@ order_router=APIRouter(
 )
 
 
+session=Session(bind=engine)
+
 @order_router.get('/')
-async def hello():
+async def hello(Authorize:AuthJWT=Depends()):
+
+    try:
+        Authorize.jwt_required()
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid Token"
+        )
     return {"message":"Hello World"}
+
+
+@order_router.post('/order',status_code=status.HTTP_201_CREATED)
+async def place_an_order(order:OrderModel,Authorize:AuthJWT=Depends()):
+    try:
+        Authorize.jwt_required()
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid Token"
+        )
+
+    current_user=Authorize.get_jwt_subject()
+
+    user=session.query(User).filter(User.username==current_user).first()
+
+
+    new_order=Order(
+        pizza_size=order.pizza_size,
+        quantity=order.quantity
+    )
+
+    new_order.user=user
+
+    session.add(new_order)
+
+    session.commit()
+
+
+    response={
+        "pizza_size":new_order.pizza_size,
+        "quantity":new_order.quantity,
+        "id":new_order.id,
+        "order_status":new_order.order_status
+    }
+
+    return jsonable_encoder(response)
+
+
+
+    
--- schemas.py
+++ schemas.py
@@ -32,3 +32,22 @@ class Settings(BaseModel):
 class LoginModel(BaseModel):
     username:str
     password:str
+
+
+
+class OrderModel(BaseModel):
+    id:Optional[int]
+    quantity:int
+    order_status:Optional[str]="PENDING"
+    pizza_size:Optional[str]="SMALL"
+    user_id:Optional[int]
+
+
+    class Config:
+        orm_mode=True
+        schema_extra={
+            "example":{
+                "quantity":2,
+                "pizza_size":"LARGE"
+            }
+        }
