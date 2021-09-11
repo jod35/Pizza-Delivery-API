1cf7ea655d4858b05571b01e16df6cd8479ce7c8
jod35 <jodestrevin@gmail.com>
2021-09-11 15:15:28 +0300

    retrieve user order

--- order_routes.py
+++ order_routes.py
@@ -89,3 +89,47 @@ async def list_all_orders(Authorize:AuthJWT=Depends()):
     raise  HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
             detail="You are not a superuser"
         )
+
+
+@order_router.get('/orders/{id}')
+async def get_order_by_id(id:int,Authorize:AuthJWT=Depends()):
+    try:
+        Authorize.jwt_required()
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid Token"
+        )
+
+    user=Authorize.get_jwt_subject()
+
+    current_user=session.query(User).filter(User.username==user).first()
+
+    if current_user.is_staff:
+        order=session.query(Order).filter(Order.id==id).first()
+
+        return jsonable_encoder(order)
+
+    raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User not alowed to carry out request"
+        )
+
+    
+@order_router.get('/user/orders')
+async def get_user_orders(Authorize:AuthJWT=Depends()):
+    try:
+        Authorize.jwt_required()
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid Token"
+        )
+
+    user=Authorize.get_jwt_subject()
+
+
+    current_user=session.query(User).filter(User.username==user).first()
+
+    return jsonable_encoder(current_user.orders)
+
